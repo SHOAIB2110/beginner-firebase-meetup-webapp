a168990bb75fdc64556066cdf4e8d89426c541cf
SHOAIB2110 <shoaibsayyad2110@gmail.com>
2020-03-01 14:33:51 +0530

    1 file modified

--- index.js
+++ index.js
@@ -101,3 +101,16 @@ form.addEventListener("submit", (e) => {
  // Return false to avoid redirect
  return false;
 });
+firebase.firestore().collection("guestbook")
+.orderBy("timestamp","desc")
+.onSnapshot((snaps) => {
+ // Reset page
+ guestbook.innerHTML = "";
+ // Loop through documents in database
+ snaps.forEach((doc) => {
+   // Create an HTML entry for each document and add it to the chat
+   const entry = document.createElement("p");
+   entry.textContent = doc.data().name + ": " + doc.data().text;
+   guestbook.appendChild(entry);
+ });
+});
